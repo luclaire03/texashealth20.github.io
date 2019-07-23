1a4ea244aae066fedc283b5f0553069d848c6dd6
texashealth20 <53234454+texashealth20@users.noreply.github.com>
2019-07-23 15:33:27 -0500

    Got Food page (first version)
    Got Food subpage, with all the images to be in the slideshow

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-// alert("hey");
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
+// IF GoogleMaps API:
+// https://developers.google.com/maps/documentation/embed/start
+// https://developers.google.com/maps/documentation/javascript/get-api-key#get-an-api-key
+// https://developers.google.com/maps/documentation/javascript/places
